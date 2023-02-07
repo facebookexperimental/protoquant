0fdb64e5fcd4aeb845c7f42b9f8ce2d2da3ae398
Christian Puhrsch <cpuhrsch@fb.com>
2023-02-07 21:47:06 +0000

    Cleanup

--- protoquant/qlinear.py
+++ protoquant/qlinear.py
@@ -1,7 +1,5 @@
 import torch
 from torch.nn.parameter import Parameter
-# from protoquant.src.triton.dequant import dequant_kernel
-# from protoquant.src.triton.quant import quant_kernel
 from protoquant.quantization import dqntz, qntz
 
 
@@ -10,11 +8,12 @@ class QLinear(torch.nn.Module):
     def __init__(self, qweight, wparams, bias):
         super(QLinear, self).__init__()
         assert isinstance(bias, Parameter)
-        self.qweight = qweight
+        # Need to store in transposed form due to cuBLAS
+        self.qweight_transposed = qweight.t().contiguous()
         self.wparams = wparams
         self.bias = bias
-        self.in_features = self.qweight.size(1)
-        self.out_features = self.qweight.size(1)
+        self.in_features = qweight.size(1)
+        self.out_features = qweight.size(1)
 
     def forward(self, inp: torch.Tensor) -> torch.Tensor:
         assert inp.dim() == 3
@@ -22,18 +21,9 @@ class QLinear(torch.nn.Module):
         inp_size1 = inp.size(1)
         inp_size2 = inp.size(2)
         inp = inp.reshape(inp_size0 * inp_size1, inp_size2)
-
         qinp, iparams = qntz(inp, is_a=True)
-
-        bias = self.bias
-        qweight = self.qweight
-        wparams = self.wparams
-
-        # d = torch.ops.protoquant._triton_gemm(qinp, qweight.t())
-        # d = torch.ops.aten._int_mm(qinp, qweight.t())
-        d = torch.mm(qinp.float(), qweight.t().float()).int()
-        # d = _matmul_call(qinp, qweight.t())
-        return dqntz(d, iparams, wparams, bias).view(inp_size0, inp_size1, -1)
+        d = torch.ops.aten._int_mm(qinp, self.qweight_transposed)
+        return dqntz(d, iparams, self.wparams, self.bias).view(inp_size0, inp_size1, -1)
 
     def extra_repr(self) -> str:
         return 'in_features={}, out_features={}, bias={}'.format(
