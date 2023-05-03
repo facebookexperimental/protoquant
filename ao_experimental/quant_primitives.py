589903266481736bca7c69252b45f9159c14e6ea
Charles Hernandez <cdhernandez@fb.com>
2023-05-03 01:30:17 +0000

    WIP Summary:
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 84e90e5005a846db70aa94d57de383d6aad960df
    Pull Request resolved: https://github.com/facebookexperimental/protoquant/pull/19

--- ao_experimental/quant_primitives.py
+++ ao_experimental/quant_primitives.py
@@ -45,6 +45,148 @@ def dynamically_quantize_per_tensor(x: torch.Tensor, quant_min: int = -128, quan
 
     return x_q, scale, zero_point
 
+def dynamically_quantize_per_channel(x: torch.Tensor, quant_min: int=-128, quant_max: int=127, target_dtype: torch.dtype = torch.int8, axis: int = 0):
+    # assumes symmetric quantization
+    # assumes dense memory format
+    # TODO(future): relax ^ as needed
+
+    # default setup for affine quantization of activations
+    eps = torch.finfo(torch.float32).eps
+
+    # get min and max
+    def get_min_max_per_channel(x: torch.Tensor, axis: int):
+        if len(x.shape)==2:
+            return torch.aminmax(x, dim = axis-1)
+        elif len(x.shape)>2:
+            x_dim = x.shape
+            new_axis_list = [i for i in range(len(x_dim))]
+            new_axis_list[axis] = 0
+            new_axis_list[0] = axis
+            x = x.permute(new_axis_list)
+            x = torch.flatten(x, start_dim = 1)
+            return torch.aminmax(x, dim = 1)
+
+    min_val, max_val = get_min_max_per_channel(x, axis=axis)
+
+    # calculate scale and zero point based on min and max
+    # reference: https://fburl.com/code/4wll53rk
+    max_val_pos = torch.max(max_val, -min_val)
+    
+    # reference: https://fburl.com/code/srbiybme
+    scale = max_val_pos / (float(quant_max - quant_min) / 2)
+    # ensure scale is the same dtype as the original tensor
+    scale = torch.clamp(scale, min=eps).to(x.dtype)
+    device = max_val_pos.device
+    zero_point = torch.zeros(max_val_pos.size(), dtype=torch.int64, device=device)
+
+    # quantize based on qmin/qmax/scale/zp
+    # reference: https://www.internalfb.com/code/fbsource/[8edc275012b1]/fbcode/caffe2/torch/ao/quantization/fx/_decomposed.py?lines=63
+    x_div = x.transpose(0, axis) / scale ## TODO HERE
+    x_round = torch.round(x_div)
+    x_zp = x_round + zero_point
+    x_zp = x_zp.transpose(0, axis) ## TODO HERE
+    quant = torch.clamp(x_zp, quant_min, quant_max).to(target_dtype)
+    # inv_scale = 1.0/scale
+    # quant = torch.clamp(torch.round(input * inv_scale) + zero_point, quant_min, quant_max).to(dtype)
+
+    return quant, scale, zero_point
+
+# reference: https://fburl.com/code/vfsygwd0
+def dequantize_per_tensor(int_repr: torch.IntTensor, scale, zero_point, out_dtype=torch.float32):
+    return (int_repr.to(out_dtype) - zero_point) * scale
+
+# reference: https://fburl.com/code/org0fmi3
+def dequantize_per_channel(int_repr: torch.IntTensor, scales, zero_points, out_dtype=torch.float32, axis: int=0):
+    # assumes axis is 0
+    y = int_repr.transpose(0, 1) ## TODO HERE
+    y = y.to(out_dtype)
+    y = y - zero_points
+    y = y * scales
+    y = y.transpose(0, 1) ## TODO HERE
+    return y
+
+def quant_int8_dynamic_linear(
+    x,
+    x_quant_min,
+    x_quant_max,
+    x_q_dtype,
+    w_vals_int8_t,
+    w_scales,
+    w_vals_int8_t_sums_int64,
+    bias,
+    out_dtype=torch.float32,
+):
+    # like F.linear, but with int8 dynamic quantization of activation,
+    # and a quantized weight
+    x_vals_int8, x_scale, x_zp = dynamically_quantize_per_tensor(
+        x, x_quant_min, x_quant_max, x_q_dtype)
+    # w_vals_int8_t_sums_int64 = w_vals_int8_t.sum(dim=0)
+    mm_out = quant_int8_matmul(
+        x_vals_int8, x_scale, x_zp, w_vals_int8_t, w_vals_int8_t_sums_int64,
+        w_scales, out_dtype)
+    if bias is not None:
+        mm_out += bias
+    return mm_out
+
+def quant_int8_matmul(
+    x_vals_int8,
+    x_scale,
+    x_zp,
+    w_vals_int8_t,
+    w_vals_int8_t_sums_int64,
+    w_scales,
+    out_dtype=torch.float32,
+):
+    # Quantized matmul of int8 operands that accumulates to int32 and returns
+    # out_dtype. For now, this is written for approximate numerical
+    # correctness, and things like aligning accumulation behaviors and
+    # performance optimizations are left for a future PR.
+    # Assumes that weight quantization is symmetric, i.e. w_zp is 0.
+    # Assumes that weight quantization is per-channel.
+    """###### Assumes that axis is 0 ## TODO CDH"""
+
+    # see
+    # https://github.com/google/gemmlowp/blob/master/doc/quantization.md
+    # for an overview of quantized matmul compute
+
+    # in scalar form, assuming out_dtype is fp32 and zw == 0:
+    #
+    #   Y_i_j_fp32 = sx * sw (dot(X_i, W_j) - zx * sum(W_j))
+    #
+
+    assert x_vals_int8.dtype in (torch.int8), \
+        f'x dtype {x_vals_int8.dtype} not yet supported'
+    assert w_vals_int8_t.dtype == torch.int8, \
+        f'w dtype {w_vals_int8_t.dtype} not yet supported'
+    assert w_scales.dtype == out_dtype, \
+        f'{w_scales.dtype} does not match {out_dtype}'
+
+    #
+    # 1. do the matrix form of dot(X_i, W_j)
+    #
+
+    # TODO(before land): add test case for input with bsz
+    tmp = x_vals_int8.reshape(-1, x_vals_int8.shape[-1])
+    y_dot_int32 = safe_int_mm(tmp, w_vals_int8_t)
+    y_dot_int32 = y_dot_int32.reshape(*x_vals_int8.shape[:-1], -1)
+
+
+    # TODO(future): consider using integer arithmetic throughout, although
+    # TBD if that is actually faster on GPUs
+    # need to use 32 bits here to prevent overflow for large shapes,
+    # 16 bits is not enough
+    y_dot_float32 = y_dot_int32.to(torch.float32)
+
+    #
+    # 2. connect it all together
+    #
+
+    # mm_unscaled has to stay in float32 for the next two lines to prevent overflow
+    mm_unscaled_float32 = (y_dot_float32 - (x_zp * w_vals_int8_t_sums_int64))
+    y = x_scale * w_scales * mm_unscaled_float32
+    # can downcast only at the very end
+    y = y.to(out_dtype)
+    return y
 
 def safe_int_mm(x_int8: torch.Tensor, w_int8: torch.Tensor):
     """
